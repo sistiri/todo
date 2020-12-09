b70fa306d312f72bd950c9e4a2e80a08f4588a76
sistiri <sistiri@yahoo.com>
2020-12-09 01:49:41 +0100

    design ok, add new / delete item ok

---
+++ js/app.js
@@ -0,0 +1,42 @@
+(function addButtonClickHandler() {
+    document.querySelector('.btn-add').addEventListener('click', (event) => {
+        addNewPendingItem();
+        deleteItemClickHandler();
+        // console.log(newPendingItemHTML(inputTextValue('input.text-input')));
+        // console.log(document.querySelector('.pending-items'));
+    });
+})();
+
+function addNewPendingItem() {
+    document.querySelector('.pending-items')
+    .insertAdjacentHTML('afterbegin', newPendingItemHTML(inputTextValue('input.text-input')));
+
+};
+
+function inputTextValue(inputSelector) {
+    return document.querySelector(inputSelector).value;
+};
+
+
+function newPendingItemHTML(string) {
+    return `<li><input class="checkbox" type="checkbox" id="${string}">
+<label for="${string}"></label>${string}<button class="btn btn-delete-item">
+<i class="fa fa-trash-o" aria-hidden="true"></i></button></li>`
+};
+
+function deleteItemClickHandler() {
+    const trashButtons = document.querySelectorAll('.btn-delete-item')
+        for ( let i = 0; i < trashButtons.length; i += 1 ) {
+            trashButtons[i].addEventListener('click', (event) => {
+                event.currentTarget.parentElement.remove();
+            // console.log(event.currentTarget.parentElement);
+        });
+        
+    };
+
+};
+
+
+
+
+
